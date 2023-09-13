a40d68dbfd9deef41a9f9e5b7c4341b56d3a8e65
KARAN RANA <karanrana1706@gmail.com>
2023-09-13 22:07:55 +0530

    4a-watermelon

---
+++ 4a-Watermelon/watermelon.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define PUB push_back
+#define POB pop_back
+#define vi vector<int>
+#define mi map<int, int>
+
+typedef unsigned long long ll;
+
+void init_code()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+}
+
+int main(void)
+{
+    FAST
+    init_code();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int w;
+        cin >> w;
+        if(w%2!=0||w==2)
+            cout<<"NO";
+        else
+            cout<<"YES";
+
+    }
+    return 0;
+}

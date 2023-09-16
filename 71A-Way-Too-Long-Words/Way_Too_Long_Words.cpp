332543e8da2adade660760a0fbd89d62eea781f1
KARAN RANA <karanrana1706@gmail.com>
2023-09-16 11:06:37 +0530

    Solved

--- 71A-Way-Too-Long-Words/Way_Too_Long_Words.cpp
+++ 71A-Way-Too-Long-Words/Way_Too_Long_Words.cpp
@@ -1,39 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-#define FAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define REP(i, a, b) for (int i = a; i < b; ++i)
-#define PUB push_back
-#define POB pop_back
-#define vi vector<int>
-#define mi map<int, int>
-
-typedef unsigned long long ll;
-
-void init_code()
+int main()
 {
-    #ifndef ONLINE_JUDGE
-        freopen("in.txt", "r", stdin);
-        freopen("out.txt", "w", stdout);
-    #endif
-}
-
-int main(void)
-{
-    FAST
-    init_code();
     int t;
-    cin >> t;
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-        int w;
-        cin >> w;
-        if(w%2!=0||w==2)
-            cout<<"NO";
-        else
-            cout<<"YES";
-
+        string s;
+        cin>>s;
+        int n=s.size();
+        if(n>10)
+        {
+            cout<<s[0]+to_string(n-2)+s[n-1]<<endl;
+        }
+        else  
+            cout<<s<<endl;
     }
-    return 0;
 }
